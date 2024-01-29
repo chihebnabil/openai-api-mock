d98ae2beafc9ca6645a9a69404ac93a15f597cfb
Nabil CHIHEB <chiheb.design@gmail.com>
2024-01-29 19:05:20 +0100

    Refractor the code for better readability

--- src/responce.js
+++ src/responce.js
@@ -1,33 +1,42 @@
 const { faker } = require('@faker-js/faker');
 
-function getResponce(requestBody) {
-    const created = Math.floor(Date.now() / 1000)
+function getResponse(requestBody) {
+    const created = Math.floor(Date.now() / 1000);
+
     if (!requestBody.functions) {
-        return {
-            choices: [
-                {
-                    finish_reason: 'stop',
-                    index: 0,
-                    message: {
-                        content: faker.lorem.words(5),
-                        role: 'assistant',
-                    },
-                    logprobs: null,
-                },
-            ],
-            created: created,
-            id: `chatcmpl-${faker.string.alphanumeric(30)}`,
-            model: `gpt-3.5-mock`,
-            object: 'chat.completion',
-            usage: {
-                completion_tokens: 17,
-                prompt_tokens: 57,
-                total_tokens: 74,
-            },
-        }
+        return createDefaultResponse(created);
     }
 
-    let functionCallingResponse = {
+    return createFunctionCallingResponse(requestBody, created);
+}
+
+function createDefaultResponse(created) {
+    return {
+        choices: [
+            {
+                finish_reason: 'stop',
+                index: 0,
+                message: {
+                    content: faker.lorem.words(5),
+                    role: 'assistant',
+                },
+                logprobs: null,
+            },
+        ],
+        created: created,
+        id: `chatcmpl-${faker.string.alphanumeric(30)}`,
+        model: `gpt-3.5-mock`,
+        object: 'chat.completion',
+        usage: {
+            completion_tokens: 17,
+            prompt_tokens: 57,
+            total_tokens: 74,
+        },
+    };
+}
+
+function createFunctionCallingResponse(requestBody, created) {
+    const functionCallingResponse = {
         id: `chatcmpl-${faker.string.alphanumeric(30)}`,
         object: 'chat.completion',
         created: created,
@@ -38,54 +47,65 @@ function getResponce(requestBody) {
                 message: {
                     role: 'assistant',
                     content: null,
-                    function_call: {
-                        name: `${requestBody.functions[0].name}`,
-                        arguments: `${generateFunctionCallArguments(requestBody)}`,
-                    }
+                    function_call: createFunctionCallObject(requestBody),
                 },
-                finish_reason: 'function_call'
-            }
+                finish_reason: 'function_call',
+            },
         ],
         usage: {
             prompt_tokens: 81,
             completion_tokens: 19,
-            total_tokens: 100
-        }
+            total_tokens: 100,
+        },
     };
+
     return functionCallingResponse;
 }
 
+function createFunctionCallObject(requestBody) {
+    return {
+        name: `${requestBody.functions[0].name}`,
+        arguments: `${generateFunctionCallArguments(requestBody)}`,
+    };
+}
+
 function generateFakeData(type, properties) {
-    if (type === 'string') {
-        return faker.lorem.words(5);
-    } else if (type === 'number') {
-        return faker.number.int();
-    } else if (type === 'array') {
-        const arrayItemsType = properties.items.type;
-        let arrayItems = [];
-        if (arrayItemsType === 'string') {
-            arrayItems = Array.from({ length: 5 }, () => faker.lorem.words(1));
-        } else if (arrayItemsType === 'object') {
-            const itemProperties = properties.items.properties;
-            arrayItems = Array.from({ length: 5 }, () => generateFakeData('object', { properties: itemProperties }));
-        }
-        return arrayItems;
-    } else if (type === 'object') {
-        const itemObject = {};
-        Object.entries(properties.properties).forEach(([itemName, itemDetails]) => {
-            itemObject[itemName] = generateFakeData(itemDetails.type, itemDetails);
-        });
-        return itemObject;
-    }else if (type === 'boolean') {
-        return faker.datatype.boolean();
-    }else {
-        return faker.lorem.words(1);
+    switch (type) {
+        case 'string':
+            return faker.lorem.words(5);
+        case 'number':
+            return faker.number.int();
+        case 'array':
+            return generateFakeArray(properties);
+        case 'object':
+            return generateFakeObject(properties);
+        case 'boolean':
+            return faker.datatype.boolean();
+        default:
+            return faker.lorem.words(1);
     }
 }
 
-/*
-* Generate function call arguments
-*/
+function generateFakeArray(properties) {
+    const arrayItemsType = properties.items.type;
+    return Array.from({ length: 5 }, () => {
+        if (arrayItemsType === 'string') {
+            return faker.lorem.words(1);
+        } else if (arrayItemsType === 'object') {
+            const itemProperties = properties.items.properties;
+            return generateFakeData('object', { properties: itemProperties });
+        }
+    });
+}
+
+function generateFakeObject(properties) {
+    const itemObject = {};
+    Object.entries(properties.properties).forEach(([itemName, itemDetails]) => {
+        itemObject[itemName] = generateFakeData(itemDetails.type, itemDetails);
+    });
+    return itemObject;
+}
+
 function generateFunctionCallArguments(requestBody) {
     const { parameters } = requestBody.functions[0];
     const argumentsObject = {};
@@ -98,5 +118,5 @@ function generateFunctionCallArguments(requestBody) {
 }
 
 module.exports = {
-    getResponce
-}
+    getResponse,
+};
