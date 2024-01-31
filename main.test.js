9c5fcb03949a0814b5d8c99a86ffb371a4979a2c
Chiheb Nabil <chiheb.design@gmail.com>
2024-01-31 12:13:13 +0100

    add function_calling unit testing

--- main.test.js
+++ main.test.js
@@ -1,8 +1,8 @@
-const { mockOpenAIResponse, stopMocking} = require('./main');
-    
+const { mockOpenAIResponse, stopMocking } = require('./main');
+
 const OpenAI = require('openai');
 const openai = new OpenAI();
-    
+
 describe('mockOpenAIResponse', () => {
     beforeEach(() => {
         mockOpenAIResponse(true);
@@ -14,15 +14,78 @@ describe('mockOpenAIResponse', () => {
         try {
             // Post using fetch
             const response = await openai.chat.completions.create({
-                    model: "gpt-3.5",
-                    messages: [
-                        { role: 'system', content: "You'r an expert chef" },
-                        { role: 'user', content: "Suggest at least 5 recipes" },
-                    ]
+                model: "gpt-3.5",
+                messages: [
+                    { role: 'system', content: "You'r an expert chef" },
+                    { role: 'user', content: "Suggest at least 5 recipes" },
+                ]
             });
-           
+
             expect(response.choices[0]).toHaveProperty('finish_reason', 'stop');
             expect(response.model).toEqual('gpt-3.5-mock');
+        } catch (error) {
+            throw new Error(error);
+        }
+    });
+
+    it('should mock the chat completion with function call', async () => {
+        try {
+            // Post using fetch
+            const response = await openai.chat.completions.create({
+                model: "gpt-3.5",
+                messages: [
+                    { role: 'system', content: "You'r an expert chef" },
+                    { role: 'user', content: "Suggest at least 5 recipes" },
+                ],
+                functions: [
+                    {
+                        name: 'get_recipes',
+                        description: 'Suggest at least 5 recipes based on the ingredients you have',
+                        parameters: {
+                            type: "object",
+                            properties: {
+                                recipes: {
+                                    type: "array",
+                                    description: "The recipes that can be made with the ingredients",
+                                    items: {
+                                        type: 'object',
+                                        properties: {
+                                            name: {
+                                                type: 'string',
+                                                description: 'The name of the recipe',
+                                            },
+                                            ingredients: {
+                                                type: 'array',
+                                                description: 'The ingredients needed for the recipe',
+                                                items: {
+                                                    type: 'string',
+                                                    description: 'The name of the ingredient',
+                                                },
+                                            },
+                                            instructions: {
+                                                type: 'string',
+                                                description: 'Detailed instructions on how to make the recipe',
+                                            },
+                                            serving: {
+                                                type: 'string',
+                                                description: 'The number of people the recipe serves',
+                                            },
+                                        }
+                                    }
+                                },
+                            }
+                        },
+                        required: ["recipes"],
+                    },
+                ],
+                function_call: { name: 'get_recipes' },
+            });
+            expect(response.model).toEqual('gpt-3.5-mock');
+            expect(response.choices[0]).toHaveProperty('finish_reason', 'function_call');
+            expect(response.choices[0].message.function_call).toHaveProperty('name', 'get_recipes');
+            expect(response.choices[0].message.function_call).toHaveProperty('arguments');
+            expect(JSON.parse(response.choices[0].message.function_call.arguments)).toHaveProperty('recipes');
+
         } catch (error) {
             throw new Error(error);
         }
@@ -32,19 +95,19 @@ describe('mockOpenAIResponse', () => {
     it('should mock image generation', async () => {
         try {
             const response = await openai.images.generate({
-                    model: "dall-e-3",
-                    prompt: "Kurt Cobain",
-                    n: 1,
-                    size: "1024x1024",
-                    quality: "hd",
+                model: "dall-e-3",
+                prompt: "Kurt Cobain",
+                n: 1,
+                size: "1024x1024",
+                quality: "hd",
             });
-     
+
             expect(response.data[0]).toHaveProperty('revised_prompt');
             expect(response.data[0]).toHaveProperty('url');
-        }catch(error){
+        } catch (error) {
             throw new Error(error);
         }
-    
+
     });
 
     it('should allow other requests', async () => {
@@ -56,7 +119,7 @@ describe('mockOpenAIResponse', () => {
             }
             const data = await response.json();
             expect(data[0]).toHaveProperty('completed');
-        }catch(error){
+        } catch (error) {
             throw new Error(error);
         }
     })
