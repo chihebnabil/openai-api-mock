425433393be183d4d23b3a514eb7a01adfd7d78a
unknown <chiheb.design@gmail.com>
2024-02-04 14:19:43 +0100

    remove .github & formatting

--- src/chat.js
+++ src/chat.js
@@ -1,147 +1,155 @@
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
 function getChatResponce(requestBody) {
-    const created = Math.floor(Date.now() / 1000);
+  const created = Math.floor(Date.now() / 1000);
 
-    if (!requestBody.functions && !requestBody.tools) {
-        return createDefaultResponse(created);
-    }
+  if (!requestBody.functions && !requestBody.tools) {
+    return createDefaultResponse(created);
+  }
 
-    return createFunctionCallingResponse(requestBody, created);
+  return createFunctionCallingResponse(requestBody, created);
 }
 
 function createDefaultResponse(created) {
-    return {
-        choices: [
-            {
-                finish_reason: 'stop',
-                index: 0,
-                message: {
-                    content: faker.lorem.words(5),
-                    role: 'assistant',
-                },
-                logprobs: null,
-            },
-        ],
-        created: created,
-        id: `chatcmpl-${faker.string.alphanumeric(30)}`,
-        model: `gpt-3.5-mock`,
-        object: 'chat.completion',
-        usage: {
-            completion_tokens: 17,
-            prompt_tokens: 57,
-            total_tokens: 74,
+  return {
+    choices: [
+      {
+        finish_reason: "stop",
+        index: 0,
+        message: {
+          content: faker.lorem.words(5),
+          role: "assistant",
         },
-    };
+        logprobs: null,
+      },
+    ],
+    created: created,
+    id: `chatcmpl-${faker.string.alphanumeric(30)}`,
+    model: `gpt-3.5-mock`,
+    object: "chat.completion",
+    usage: {
+      completion_tokens: 17,
+      prompt_tokens: 57,
+      total_tokens: 74,
+    },
+  };
 }
 
 function createFunctionCallingResponse(requestBody, created) {
-    const isTool = Boolean(requestBody.tools);
-    const functionOrToolCallObject = isTool ? [createToolCallObject(requestBody)] : createFunctionCallObject(requestBody);
-    const functionOrToolCall = isTool ? 'tool_calls' : 'function_call';
+  const isTool = Boolean(requestBody.tools);
+  const functionOrToolCallObject = isTool
+    ? [createToolCallObject(requestBody)]
+    : createFunctionCallObject(requestBody);
+  const functionOrToolCall = isTool ? "tool_calls" : "function_call";
 
-    const functionCallingResponse = {
-        id: `chatcmpl-${faker.string.alphanumeric(30)}`,
-        object: 'chat.completion',
-        created: created,
-        model: 'gpt-3.5-mock',
-        choices: [
-            {
-                index: 0,
-                message: {
-                    role: 'assistant',
-                    content: null,
-                    [functionOrToolCall]: functionOrToolCallObject,
-                },
-                finish_reason: functionOrToolCall,
-            },
-        ],
-        usage: {
-            prompt_tokens: 81,
-            completion_tokens: 19,
-            total_tokens: 100,
+  const functionCallingResponse = {
+    id: `chatcmpl-${faker.string.alphanumeric(30)}`,
+    object: "chat.completion",
+    created: created,
+    model: "gpt-3.5-mock",
+    choices: [
+      {
+        index: 0,
+        message: {
+          role: "assistant",
+          content: null,
+          [functionOrToolCall]: functionOrToolCallObject,
         },
-    };
+        finish_reason: functionOrToolCall,
+      },
+    ],
+    usage: {
+      prompt_tokens: 81,
+      completion_tokens: 19,
+      total_tokens: 100,
+    },
+  };
 
-    return functionCallingResponse;
+  return functionCallingResponse;
 }
 
 function createToolCallObject(requestBody) {
-    return {
-        id: `call-${faker.string.alphanumeric(30)}`,
-        type: "function",
-        "function": {
-            name: `${requestBody.tools[0].function.name}`,
-            arguments: `${generateToolCallArguments(requestBody)}`
-        }
-    };
+  return {
+    id: `call-${faker.string.alphanumeric(30)}`,
+    type: "function",
+    function: {
+      name: `${requestBody.tools[0].function.name}`,
+      arguments: `${generateToolCallArguments(requestBody)}`,
+    },
+  };
 }
 function createFunctionCallObject(requestBody) {
-    return {
-        name: `${requestBody.functions[0].name}`,
-        arguments: `${generateFunctionCallArguments(requestBody)}`,
-    };
+  return {
+    name: `${requestBody.functions[0].name}`,
+    arguments: `${generateFunctionCallArguments(requestBody)}`,
+  };
 }
 
 function generateToolCallArguments(requestBody) {
-    const { parameters } = requestBody.tools[0].function;
-    const argumentsObject = {};
+  const { parameters } = requestBody.tools[0].function;
+  const argumentsObject = {};
 
-    Object.entries(parameters.properties).forEach(([paramName, paramDetails]) => {
-        argumentsObject[paramName] = generateFakeData(paramDetails.type, paramDetails);
-    });
+  Object.entries(parameters.properties).forEach(([paramName, paramDetails]) => {
+    argumentsObject[paramName] = generateFakeData(
+      paramDetails.type,
+      paramDetails
+    );
+  });
 
-    return JSON.stringify(argumentsObject, null, 2);
+  return JSON.stringify(argumentsObject, null, 2);
 }
 
 function generateFakeData(type, properties) {
-    switch (type) {
-        case 'string':
-            return faker.lorem.words(5);
-        case 'number':
-            return faker.number.int();
-        case 'array':
-            return generateFakeArray(properties);
-        case 'object':
-            return generateFakeObject(properties);
-        case 'boolean':
-            return faker.datatype.boolean();
-        default:
-            return faker.lorem.words(1);
-    }
+  switch (type) {
+    case "string":
+      return faker.lorem.words(5);
+    case "number":
+      return faker.number.int();
+    case "array":
+      return generateFakeArray(properties);
+    case "object":
+      return generateFakeObject(properties);
+    case "boolean":
+      return faker.datatype.boolean();
+    default:
+      return faker.lorem.words(1);
+  }
 }
 
 function generateFakeArray(properties) {
-    const arrayItemsType = properties.items.type;
-    return Array.from({ length: 5 }, () => {
-        if (arrayItemsType === 'string') {
-            return faker.lorem.words(1);
-        } else if (arrayItemsType === 'object') {
-            const itemProperties = properties.items.properties;
-            return generateFakeData('object', { properties: itemProperties });
-        }
-    });
+  const arrayItemsType = properties.items.type;
+  return Array.from({ length: 5 }, () => {
+    if (arrayItemsType === "string") {
+      return faker.lorem.words(1);
+    } else if (arrayItemsType === "object") {
+      const itemProperties = properties.items.properties;
+      return generateFakeData("object", { properties: itemProperties });
+    }
+  });
 }
 
 function generateFakeObject(properties) {
-    const itemObject = {};
-    Object.entries(properties.properties).forEach(([itemName, itemDetails]) => {
-        itemObject[itemName] = generateFakeData(itemDetails.type, itemDetails);
-    });
-    return itemObject;
+  const itemObject = {};
+  Object.entries(properties.properties).forEach(([itemName, itemDetails]) => {
+    itemObject[itemName] = generateFakeData(itemDetails.type, itemDetails);
+  });
+  return itemObject;
 }
 
 function generateFunctionCallArguments(requestBody) {
-    const { parameters } = requestBody.functions[0];
-    const argumentsObject = {};
+  const { parameters } = requestBody.functions[0];
+  const argumentsObject = {};
 
-    Object.entries(parameters.properties).forEach(([paramName, paramDetails]) => {
-        argumentsObject[paramName] = generateFakeData(paramDetails.type, paramDetails);
-    });
+  Object.entries(parameters.properties).forEach(([paramName, paramDetails]) => {
+    argumentsObject[paramName] = generateFakeData(
+      paramDetails.type,
+      paramDetails
+    );
+  });
 
-    return JSON.stringify(argumentsObject, null, 2);
+  return JSON.stringify(argumentsObject, null, 2);
 }
 
 module.exports = {
-    getChatResponce,
+  getChatResponce,
 };
